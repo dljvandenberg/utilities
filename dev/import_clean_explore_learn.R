75dfae806a6ce19d03c655460e473df07d4687b9
dljvandenberg <dljvandenberg@gmail.com>
2015-12-15 12:42:15 +0100

    Explore univariate linear models

--- dev/import_clean_explore_learn.R
+++ dev/import_clean_explore_learn.R
@@ -87,6 +87,18 @@ ggpairs(mtcars)
 #hCluster = hclust(dist(t(trainSpam[, 1:57])))
 #plot(hCluster)
 
+# Try multiple univariate linear models and find predictor with minimum cross-validated error
+#trainSpam$numType = as.numeric(trainSpam$type) - 1
+#costFunction = function(x, y) sum(x != (y > 0.5))
+#cvError = rep(NA, 55)
+#library(boot)
+#for (i in 1:55) {
+#    lmFormula = reformulate(names(trainSpam)[i], response = "numType")
+#    glmFit = glm(lmFormula, family = "binomial", data = trainSpam)
+#    cvError[i] = cv.glm(trainSpam, glmFit, costFunction, 2)$delta[2]
+#}
+#names(trainSpam)[which.min(cvError)]
+
 
 ## INFERENCE
 
