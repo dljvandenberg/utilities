b10dceae123db4927727a9b5dfa2117d70791497
dljvandenberg <dljvandenberg@gmail.com>
2016-01-03 22:23:18 +0100

    Create cacher.R

---
+++ dev/cacher.R
@@ -0,0 +1,9 @@
+# cacher package for creating and inspecting cached computations
+library(cacher)
+clonecache(id = "<objectsignature>")
+showfiles()
+sourcefile("<filename>")
+objectcode("<object>")
+runcode()
+checkcode()
+loadcache()
